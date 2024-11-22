39e2851a5cc5554c7a3d63687dfd47b81bc4291f
dfad <dfad>
2024-11-22 14:54:55 +0500

    docs(iorder_handler.py): Добавление описания аргументов

--- interfaces/iorder_handler.py
+++ interfaces/iorder_handler.py
@@ -1,13 +1,28 @@
 from abc import ABC, abstractmethod
 
+
 class IOrderHandler(ABC):
     """Интерфейс для обработчиков цепочки обязанностей."""
+
     @abstractmethod
     def set_next(self, handler: 'IOrderHandler') -> 'IOrderHandler':
-        """Установить следующий обработчик в цепочке."""
+        """
+        Установить следующий обработчик в цепочке.
+
+        Args:
+            handler (IOrderHandler): Следующий обработчик в цепочке обязанностей.
+
+        Returns:
+            IOrderHandler: Установленный следующий обработчик.
+        """
         pass
 
     @abstractmethod
     def handle(self, order: dict) -> None:
-        """Обработать заказ."""
+        """
+        Обработать заказ.
+
+        Args:
+            order (dict): Словарь с данными заказа, включающий информацию о товаре, платеже и адресе.
+        """
         pass
